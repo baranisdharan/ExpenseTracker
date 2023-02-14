2653bf4fc11a8a575a617b219b9965ee691d1c37
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-02-15 01:42:53 +0530

    Expense Changer 100 Sharpener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,25 @@
+import React , {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from './../UI/Card';
 
 const ExpenseItem = (props) => {  
+  let [ title , setTitle ] = useState(props.title)
+  let [ amount , setAmount ] = useState(props.amount)
+  let clickHandler = () => {
+    setTitle("updated")
+  }
+  let clickHandler2 = () => {
+    setAmount("100")
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails amount={props.amount} title={props.title} />
-      <button onClick={()=> console.log("Deleted")}>Delete Expense</button>   
+      <ExpenseDetails amount={amount} title={title} />
+      <button onClick={clickHandler}>Change Title</button> 
+      <button onClick={clickHandler2}>Expense Changer</button>  
     </Card>
   );
 }
