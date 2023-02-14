ddc01c4314836d0173156eb7d708aa282cdc6a0a
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-02-14 18:57:54 +0530

    Card Composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import Card from "../Card";
 
 function ExpenseItem(props) {    
     
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails amount={props.amount} title={props.title} />      
-    </div>
+    </Card>
   );
 }
 
