34a391c4f0060b5947e94756ee9de99d145ce1bc
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-02-14 12:58:09 +0530

    first comit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Expense Items</h2>
+      </div>
+      <div>Food Rs 100</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
