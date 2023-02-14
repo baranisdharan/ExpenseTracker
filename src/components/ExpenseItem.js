17adce7da8b7e02b13c4933971339e3afcfc676c
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-02-14 14:46:41 +0530

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    let expenseDate = new Date(2023,1,14);
-    let expenseTitle = "Car Insurance"
-    let expenseAmount = 100
-    let locationOfExpenditure = "New York, NY"
+function ExpenseItem(props) {    
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2 className="expense-item">{expenseTitle}</h2>
-        <h2 className="expense-item">{locationOfExpenditure}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2 className="expense-item">{props.title}</h2>
+        <h2 className="expense-item">{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
